e2b7c54cdcf0814d7419e32a04fd414a947dfc04
KienLee0207 <46680892+KienLee0207@users.noreply.github.com>
2019-04-10 22:59:33 +0700

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,28 +1,55 @@
 package sample;
 
-public abstract class Controller {
-    int id;
-    String name;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-    public int getId() {
-        return id;
+import java.util.HashSet;
+
+public class Controller {
+    public TextField txt;
+    public Button btn;
+    public TextArea print;
+    Integer a;
+
+    public boolean isSoNguyenTo(int x){
+        int count = 0;
+        for (int i = 1;i <= x; i++){
+
+            if (x%i==0){
+                count++;
+            }
+        }
+        if (count == 2){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void submit(){
+            String alert="Nhập 1 số lớn hơn 1";
+            if(txt.getText().isEmpty()){
+                print.setText(alert);
+                return;
+            } else{
+                String str="";
+                String input = txt.getText().trim();
+                try {
+                     a =Integer.parseInt(input);
+                }catch (NumberFormatException e){
+                    txt.setText("Chỉ được nhập số không được nhập kí tự");
+                    return;
+                }for (int i=2;i<a;i++){
+                    if(this.isSoNguyenTo(i)){
+                        str+=i +"\n";
+
+                    }
+
+                }
+                print.setText(str);
+            }
+
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Controller(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public abstract void toStringg();
 }
