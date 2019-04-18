e689794ffe1196855989159797e29aad2a6f714f
KienLee0207 <46680892+KienLee0207@users.noreply.github.com>
2019-04-18 22:57:06 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,49 +1,78 @@
-package sessiom7;
+package session_6;
+import jdk.nashorn.internal.objects.annotations.Where;
 
+import java.net.ConnectException;
+import java.sql.*;
+import java.util.Collections;
 import java.util.Scanner;
-public class main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        action PP = new action();
-        boolean keepRuning = true;
-        int choosen = 111;
-        while (keepRuning) {
-            System.out.println("\n 1. Add student. \n" +
-                    "2. Edit student by id.\n" +
-                    " 3. Delete student by id. \n" +
-                    "4. Sort student by gpa. \n" +
-                    "5. Sort student by name. \n" +
-                    "6. Show student. \n" +
-                    "0. Exit.");
-            System.out.println("hãy nhập số từ 0-6: ");
-            choosen = sc.nextInt();
 
-            switch(choosen){
-                case 0 :
-                    System.out.println("chương trình kết thúc!");
-                    keepRuning = false;
-                    break;
-                case 1 :
-                    PP.addStudent();
-                    break;
-                case 2 :
-                    PP.editByID();
-                    break;
-                case 3 :
-                    PP.DeleteByID();
-                case 4 :
-                    PP.sortByGpa();
-                    break;
-                case 5 :
-                    PP.sortByName();
-                    break;
-                case 6 :
-                    PP.ShowList();
-                    break;
-                default :
-                    System.out.println("chỉ được nhập các số từ 0-6");
-                    break;
+public class main {
+    static boolean stop = false;
+    static Statement statement;
+
+    public static void main(String[] args) {
+            try {
+                //đăng kí drive
+                Class.forName("com.mysql.jdbc.Driver");
+                String URL = "jdbc:mysql://localhost:3306/t1807m";
+                Connection bo = DriverManager.getConnection(URL, "t1807m", "kienlee123");
+                //bước 4: try vấn dữ liệu
+
+                String sql = "SELECT * FROM student ";
+                statement = bo.createStatement();
+                statement.executeQuery(sql);
+                ResultSet resultSet = statement.executeQuery(sql);
+                while (resultSet.next()) {
+                    System.out.println("ID:" + resultSet.getInt("id"));
+                    System.out.println("Name:" + resultSet.getString("name"));
+                    System.out.println("Age:" + resultSet.getInt("age"));
+                    System.out.println("Salary:" + resultSet.getInt("salary"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            int start;
+            int id;
+            String name;
+            int age;
+            int salary;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\n mời bạn chọn 1 phương thức");
+            System.out.println("1: Edit student");
+            System.out.println("2: Delete student");
+            start = scanner.nextInt();
+            switch (start) {
+                case 1:
+
+                    System.out.println("id");
+                    id = scanner.nextInt();
+                    System.out.println("name");
+                    name = scanner.nextLine();
+                    scanner.nextLine();
+                    System.out.println("age");
+                    age = scanner.nextInt();
+                    System.out.println("salary");
+                    salary = scanner.nextInt();
+                    String edit_sql = "UPDATE student SET nam='"+name+"',age='"+age+"',salary='"+salary+"' WHERE id='"+id+"'";
+
+                    try {
+                        statement.executeUpdate(edit_sql);
+
+                    } catch (SQLException e) {
+                        e.getStackTrace();
+
+                    }
+                    break;
+                case 2:
+                    System.out.println("nhập ID muốn xóa");
+                    id = scanner.nextInt();
+                    String delete_sql = "DELETE FROM student WHERE id='"+id+"'";
+                    try {
+                        statement.executeUpdate(delete_sql);
+                        System.out.println("đã xóa thành công !!!");
+                    }catch (Exception e){}
+            }
+
+
         }
     }
-}
